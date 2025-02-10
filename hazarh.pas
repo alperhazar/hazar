2b7194bf6fbaa9f17cad47686e81815057d909e8
Alper Hazar AKYOL <alperhazarakyol@gmail.com>
2025-02-10 08:26:41 +0300

    Update hazarh.pas

--- hazarh.pas
+++ hazarh.pas
@@ -8,7 +8,7 @@ uses
 type
   THazarH = class(THazar) { This algorithm depends on Hazar8 or Hazar64 while build. }
     private               { HAZAR8 or HAZAR64 compiler directives must be defined at }
-      FHash: THazarData;  { main hazar.pas file/unit. }
+      FHash: THazarData;  { main hazar.pas unit. }
     public
       constructor Start(Seed: THazarData; Length: THazarInteger);
       function Feed(Seed: THazarData): THazarData;
@@ -29,7 +29,7 @@ function THazarH.Feed(Seed: THazarData): THazarData;
 var
   I: THazarInteger;
 begin
-  for I := $00 to High(Seed) do
+  for I := $00 to DataMax do
     FHash[I] := FHash[I] xor Seed[I];
   ApplyOTP(FHash);
   Result := FHash;
